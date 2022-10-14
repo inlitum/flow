a3e7213079f7dd3522f25c65bb095ce93aeb0eec
Jack Borrie <lunarinlitum@hotmail.com>
2022-10-14 17:46:05 +1300

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FlowComponent } from './components/flow/flow.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FlowComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/flow/flow.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-flow',
+  templateUrl: './flow.component.html'
+})
+export class FlowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
